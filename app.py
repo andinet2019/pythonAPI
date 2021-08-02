ae1633f77a07ca06a4c41d3d1d71d9183fb3ccd3
andinet2029 <andinetmn@gmail.com>
2021-08-01 21:08:39 -0500

    all

---
+++ app.py
@@ -0,0 +1,47 @@
+
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'name': "task1",
+        "description": "This is task 1"
+    },
+    {
+        "id": 2,
+        "name": "task2",
+        "description": "This is task 2"
+    },
+    {
+        "id": 3,
+        "name": "task3",
+        "description": "This is task 3"
+    }
+]
+
+tasksJSON = json.dumps(tasks)
+
+
+@app.route('/')
+def home():
+    return "App Works!!!"
+
+
+@app.route('/api/tasks')
+def tasks():
+    return tasksJSON
+
+@app.route('/api/task', methods=['POST'])
+def create_task():
+    request_data = request.get_json()
+    tasksData = json.loads(tasksJSON)
+    tasksData.append(request_data['task'])
+    return json.dumps(tasksData);
+
+
+
+
+
